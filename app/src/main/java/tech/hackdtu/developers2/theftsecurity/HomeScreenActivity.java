60ffb14e0f3728bdd80a91b1701cd761bfcaed3f
ishaandhamija <ishaan.dhamija@gmail.com>
2017-10-14 04:28:16 +0530

    initial commit

---
+++ app/src/main/java/tech/hackdtu/developers2/theftsecurity/HomeScreenActivity.java
@@ -0,0 +1,13 @@
+package tech.hackdtu.developers2.theftsecurity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+    }
+}
